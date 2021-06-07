ab38a74f8ad0ad04561418d6da375572e1a1cddd
subodhkum20 <subodhkum20@iitk.ac.in>
2021-06-07 13:13:32 +0530

    mongo node basic crud operations

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,31 +1,52 @@
 const mongoClient = require('mongodb').MongoClient;
-const http = require('http');
-const morgan=require('morgan');
-const bodyParser=require('body-parser');
-const express=require('express');
-// const json = require('json');
-const path = require('path');
 const assert = require('assert');
-const homeRouter = require('./routes/homeRouter');
-const url='mongodb://localhost:27017/';
-mongoClient.connect(url,{ useUnifiedTopology: true},(err,client) => {
-    assert.equal(err,null);
-    const db=client.db('items');
-    const user=db.collection('items');
-    user.insertOne({
-        "name": "subodh",
-        "subject": "javascript"
-    },(err,result) => {
-        console.log(`insert one;${result.ops}`);
-    })
+const http = require('http');
+const express = require('express');
+const dboper = require('./operations/opera');
+const url = 'mongodb://localhost:27017/';
+mongoClient.connect(url).then((client) => {
+
+    console.log('Connected correctly to server');
+    const db = client.db('test');
+
+    return dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+        "dishes")
+        .then((result) => {
+            console.log("Insert Document:\n", result.ops);
+
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Documents:\n", docs);
+
+            return dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes");
+
+        })
+        .then((result) => {
+            console.log("Updated Document:\n", result.result);
+
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Updated Documents:\n", docs);
+                            
+            return db.dropCollection("dishes");
+        })
+        .then((result) => {
+            console.log("Dropped Collection: ", result);
+
+            return client.close();
+        })
+        .catch((err) => console.log(err));
 
 })
-
-const port=3000;
-const hostname= 'localhost';
+.catch((err) => console.log(err));
+const port = 3000;
+const hostname = 'localhost';
 // app.use=server;
 const app = express();
-const server= http.createServer(app);
-app.use('/home',homeRouter);
+const server = http.createServer(app);
+// app.use('/home',homeRouter);
 
 server.listen(port, hostname)
