05107ef98d4940f2d8c3890b6632f3a9b5c20b86
subodhkum20 <80281409+subodhkum20@users.noreply.github.com>
2021-07-03 01:39:12 +0530

    Delete config.js

--- confusion_server/config.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    "mongoUrl":"mongodb://localhost:27017/",
-    "secretKey":"12345-67890-09876-54321",
-    facebook:{
-        clientId:"3038746573020443",
-        clientSecret:"a93897d5e84a8c58566c3ea9be099c70"
-    }
-}
